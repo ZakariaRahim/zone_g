afe6101fd33d406cdc0d513d4e5671082241b84b
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-07-28 17:33:06 +0000

    Update _app.php
    delete unused codes

--- api/App/Routes/_app.php
+++ api/App/Routes/_app.php
@@ -6,7 +6,3 @@ $app->get("/", function () {
     json(["message" => "Congrats!! You're on Leaf API"], 200);
 });
 
-$app->get("/app", function () {
-    // app() returns $app
-    json(app()->routes(), 200);
-});
